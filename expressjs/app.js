894ddd0a8f680dff9f2492da87da1e87dd6c6795
asdf <tayeb1293@yahoo.com>
2023-10-03 06:35:23 +0200

    expressjs practice

---
+++ expressjs/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({
+        "data": "test server",
+    })
+})
+
+app.listen(3000, ()=> {
+    console.log('server is listening to requests');
+})
