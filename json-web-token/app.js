67e4d1c2119a6de894f198b0f59d752ad72c66c1
asdf <tayeb1293@yahoo.com>
2023-12-08 11:38:44 +0200

    user login

--- json-web-token/app.js
+++ json-web-token/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
+
+app.use(express.json())
 
 const posts = [
     {
@@ -15,12 +18,48 @@ const posts = [
         title: "Post 2"
     }
 ]
+
+const users = [
+    {
+        username:"Eltayeb",
+        password: "password"
+    }
+]
 const port = process.env.PORT;
 const url = process.env.database_url;
 // console.log(url, port);
 
-app.get('/posts', (req, res)=> {
-    res.json(posts);
+app.get('/users', (req, res)=> {
+    res.json(users);
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const user = {username: req.body.username, password: hashedPassword}
+        users.push(user);
+        res.status(201).json(user);
+        
+    } catch (error) {
+        res.send(error.message);
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.username = req.body.username);
+    if(user == null){
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        if( await bcrypt.compare(req.body.password, user.password))
+            res.send('login success')
+        else 
+            res.send('enter the password correctly')
+
+    } catch (error) {
+        
+    }
 })
 
 app.listen(port, ()=> {
