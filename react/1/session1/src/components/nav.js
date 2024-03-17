a325dcede0a02fa00fab82b8a2a32a5b8ebb0688
asdf <tayeb1293@yahoo.com>
2024-03-17 17:12:36 +0200

    react

---
+++ react/1/session1/src/components/nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <>
+            <a href="#">Link1</a>
+            <a href="#">Link2</a>
+            <a href="#">Link3</a>
+            <a href="#">Link4</a>
+        </>
+    )
+}
+
+export default Nav;
