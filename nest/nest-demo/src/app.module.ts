9d37a629658f3970088410c4db47c4cd41f10c51
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-05-16 06:07:58 +0200

    nest

--- nest/nest-demo/src/app.module.ts
+++ nest/nest-demo/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContestModule } from './contest/contest.module';
 
 @Module({
-  imports: [],
+  imports: [ContestModule],
   controllers: [AppController],
   providers: [AppService],
 })
