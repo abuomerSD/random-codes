377522e51af595725d2a2aa0f114ee147bd1caec
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-05-27 06:57:41 +0200

    cs

---
+++ dotnet/learning-csharp/Person.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorld {
+	class Person {
+		public string name;
+		public int age;
+		
+		public Person(string name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+	}
+}
--- dotnet/learning-csharp/Program.cs
+++ dotnet/learning-csharp/Program.cs
@@ -6,6 +6,29 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Test");
+            int x = 10;
+            bool y = false;
+            const double z = 20.54;
+
+            Console.WriteLine($"{x} {y} {z}");
+            long l = 1000000L;
+            float f = 10.392F;
+            double d = 2309.322D;
+            Console.WriteLine(l);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            float f1 = 35e3F;
+            double d1 = 12E4D;
+            Console.WriteLine(f1);
+            Console.WriteLine(d1);
+            var person = new Person("eltayeb", 30);
+            Console.WriteLine(person.name);
+
+
+            double doub = 10.2123;
+            int inte = (int) doub;
+
+            Console.WriteLine(inte);
         }
     }
 }
