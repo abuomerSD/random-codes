e4ea0cbf805dbd3f02c1b2288f8ae3effa386590
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-05-28 06:18:36 +0200

    cs

--- dotnet/learning-csharp/Program.cs
+++ dotnet/learning-csharp/Program.cs
@@ -5,30 +5,62 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            int x = 10;
-            bool y = false;
-            const double z = 20.54;
+            // Console.WriteLine("Test");
+            // int x = 10;
+            // bool y = false;
+            // const double z = 20.54;
 
-            Console.WriteLine($"{x} {y} {z}");
-            long l = 1000000L;
-            float f = 10.392F;
-            double d = 2309.322D;
-            Console.WriteLine(l);
-            Console.WriteLine(f);
-            Console.WriteLine(d);
-            float f1 = 35e3F;
-            double d1 = 12E4D;
-            Console.WriteLine(f1);
-            Console.WriteLine(d1);
-            var person = new Person("eltayeb", 30);
-            Console.WriteLine(person.name);
+            // Console.WriteLine($"{x} {y} {z}");
+            // long l = 1000000L;
+            // float f = 10.392F;
+            // double d = 2309.322D;
+            // Console.WriteLine(l);
+            // Console.WriteLine(f);
+            // Console.WriteLine(d);
+            // float f1 = 35e3F;
+            // double d1 = 12E4D;
+            // Console.WriteLine(f1);
+            // Console.WriteLine(d1);
+            // var person = new Person("eltayeb", 30);
+            // Console.WriteLine(person.name);
 
 
-            double doub = 10.2123;
-            int inte = (int) doub;
+            // double doub = 10.2123;
+            // int inte = (int) doub;
 
-            Console.WriteLine(inte);
+            // Console.WriteLine(inte);
+
+            // int myInt = 10;
+            // double myDouble = 20.33;
+            // bool myBoolean = false;
+            // Console.WriteLine(Convert.ToString(myInt));
+            // Console.WriteLine(Convert.ToString(myBoolean));
+            // Console.WriteLine(Convert.ToDouble(myInt));
+            
+            // Console.WriteLine("Enter Your Username");
+            // string username = Console.ReadLine();
+            // Console.WriteLine(username);
+
+            // Console.WriteLine(Math.Round(0233.344));
+            // string mystr = "Hello World";
+            // int myInt = 10;
+            // Console.WriteLine(mystr.Length);
+
+            // string mystr = "Hello World";
+            // int myInt = 10;
+
+            // Console.WriteLine(mystr + myInt);
+
+            // string name = "John Doe";
+            // int charPos = name.IndexOf('D');
+            // Console.WriteLine(charPos);
+
+            // string lastName = name.Substring(charPos);
+            // Console.WriteLine(lastName);
+
+            int time = 20;
+            string result = (time < 18) ? "day" : "night";
+            Console.WriteLine(result);
         }
     }
 }
