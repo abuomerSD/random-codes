a69e3f31eaddecb63c2811ae7bd732ba62383b68
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-06-10 12:06:05 +0200

    dotnet

--- dotnet/EmployeeManagement/Program.cs
+++ dotnet/EmployeeManagement/Program.cs
@@ -1,6 +1,30 @@
+using Microsoft.AspNetCore.Http.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+var key1 = app.Configuration.GetValue<string>("MyKey");
+
+app.MapGet("/", () => key1);
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+app.Use(
+    async (context, next) =>
+    {
+        Console.WriteLine("middleware1");
+        await next.Invoke();
+    }
+);
+
+app.Use(
+    async (context, next) =>
+    {
+        Console.WriteLine("middleware2");
+        await context.Response.WriteAsync("middleware2");
+        await next.Invoke();
+    }
+);
 
 app.Run();
